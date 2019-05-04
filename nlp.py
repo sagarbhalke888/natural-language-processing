3417f5383524c64d0c60d65add31c7f9e541c096
sagarbhalke888 <47658130+sagarbhalke888@users.noreply.github.com>
2019-05-05 00:36:27 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,9 +1,9 @@
 from nltk.corpus import stopwords
 import nltk
-from nltk.stem import PorterStemmer
+
 from nltk import word_tokenize,sent_tokenize
-import pandas as pd
-ps=PorterStemmer()
+
+
 mytext= open('mahal.txt').read()
 
 
@@ -19,3 +19,5 @@ for i in sent_tokenized:
 
 #IN THIS CODE WE HAVE PERFORMED SENTENCE TOKENIXZING AS WELL AS WORD TOKENIZING
 #WE HAD FOUND OUT THE TAGS OF EACH SENTENCE IN THE GIVEN CODE
+#YOU MAKE ANY TEXT FILE OF NAME MAHAL FOR THE FOLLOWING CODE TO RUN 
+#OR YOU COULD JUST CHANGE THE NAME OF THE FILE IN THE GIVEN CODE CORRESPIONDING TO YOUR FILE NAME
